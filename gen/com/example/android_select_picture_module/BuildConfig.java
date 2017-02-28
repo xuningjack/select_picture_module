cf380f796def0fe5d47e815acfbaa62a763eff18
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-02-28 17:48:46 +0800

    delete bin and gen

--- gen/com/example/android_select_picture_module/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android_select_picture_module;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
